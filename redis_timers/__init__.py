e4fabdb1d216f82bf9ad28d48d5785743fc4f708
Artur Shiriev <me@shiriev.ru>
2025-12-16 18:43:56 +0300

    fix

--- redis_timers/__init__.py
+++ redis_timers/__init__.py
@@ -1,7 +1,12 @@
 from redis_timers.handler import Handler
-from redis_timers.lock import consume_lock, timer_lock
+from redis_timers.lock import consume_lock
 from redis_timers.router import Router
 from redis_timers.timers import Timers
 
 
-__all__ = ["Handler", "Router", "Timers", "consume_lock", "timer_lock"]
+__all__ = [
+    "Handler",
+    "Router",
+    "Timers",
+    "consume_lock",
+]
